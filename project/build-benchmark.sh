de1e01cb82f4cea27005c26129b7259b8dfb44ab
viloz1 <simonjohansson312@gmail.com>
2023-05-06 10:59:35 +0200

    improved scripts

--- project/build-benchmark.sh
+++ project/build-benchmark.sh
@@ -3,6 +3,9 @@ iterations=$2
 
 current=$(pwd)
 
+resultsfile="${path:2}"
+resultsfile=$(printf '%s\n' $resultsfile | tr '/' '_')
+
 cd $path
 
 total=0
@@ -15,9 +18,18 @@ do
     make build
     end=`date +%s.%N`
     time=$( echo "$end - $start" | bc -l )
-    values+=("'$time'")
+    values+=("$time")
     total=$( echo "$total + $time" | bc -l )
 done
+
 cd $current
-printf '%s\n' $values > ./build-results/test.txt
+
+if [ ! -d ./build_results ]; then
+  mkdir -p ./build_results;
+fi
+
+
+file="./build_results/${resultsfile}-${iterations}"
+touch $file
+printf '%s\n' "${values[@]}" > "$file"
 echo $total / $iterations | bc -l
--- project/run-benchmark.sh
+++ project/run-benchmark.sh
@@ -3,6 +3,12 @@ iterations=$2
 target=$3
 
 total=0
+values=()
+
+current=$(pwd)
+
+resultsfile="${path:2}"
+resultsfile=$(printf '%s\n' $resultsfile | tr '/' '_')
 
 cd $path
 
@@ -14,7 +20,19 @@ do
     start=`date +%s.%N`
     $target "${@:4}"
     end=`date +%s.%N`
-    runtime=$( echo "$end - $start" | bc -l )
-    total=$( echo "$total + $runtime" | bc -l )
+    time=$( echo "$end - $start" | bc -l )
+    values+=("$time")
+    total=$( echo "$total + $time" | bc -l )
 done
+
+cd $current
+
+if [ ! -d ./build_results ]; then
+  mkdir -p ./build_results;
+fi
+
+
+file="./build_results/${resultsfile}-${iterations}"
+touch $file
+printf '%s\n' "${values[@]}" > "$file"
 echo $total / $iterations | bc -l
